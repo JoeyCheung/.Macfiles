51ba62270744ca1e1143d532bca1cc50df2830d6
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-08-18 12:23:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install node
-
 brew install wget
+brew install python3
 
 #install anaconda
 cd $HOME
